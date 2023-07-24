3fd95c2eebe6c016eb353840b68875e2a64d0d6c
rtake <rtake1110@gmail.com>
2023-07-25 00:07:22 +0900

    Messageコンポーネントを作成

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Name from './components/Name'
+import Message from './components/Message'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Name />
+    <Message />
   </React.StrictMode>,
   document.getElementById('root')
 );
