2ac78c6a4f25efe1576ec8ffa6d513edf60c0b96
rtake <rtake1110@gmail.com>
2023-07-30 18:43:41 +0900

    componentにCounterを追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Parent from './components/ContainerSample'
+import Counter from './components/Counter'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Parent />
+    <Counter initialValue={1}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
