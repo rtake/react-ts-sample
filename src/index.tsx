333599857faec2445ca6d1fa0f615ea602b2c717
rtake <rtake1110@gmail.com>
2023-07-30 20:55:22 +0900

    index.tsxからCounterを削除

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import { UseMemoSample } from './components/UseMemoSample'
-import Counter from './components/Counter'
 
 ReactDOM.render(
   <UseMemoSample />,
