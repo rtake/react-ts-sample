6c430c5fdf89a1bdc9a9cea1ad5b08175da955c5
rtake <rtake1110@gmail.com>
2023-07-30 20:11:24 +0900

    UseMemoSampleコンポーネントを追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import { Parent } from './components/Parents'
+import { UseMemoSample } from './components/UseMemoSample'
 import Counter from './components/Counter'
 
 ReactDOM.render(
-  <Parent />,
+  <UseMemoSample />,
   document.getElementById('root')
 );
