3042070e1ddd45801e4eda977070f36b40d04a50
rtake <rtake1110@gmail.com>
2023-07-30 18:53:58 +0900

    CounterコンポーネントをuseReducerを使って書き換え

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,21 @@
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
+
+type Action = 'DECREMENT' | 'INCREMENT' | 'DOUBLE' | 'RESET'
+
+const reducer = (currentCount: number, action: Action) => {
+    switch (action) {
+        case 'INCREMENT':
+            return currentCount+1;
+        case 'DECREMENT':
+            return currentCount-1;
+        case 'DOUBLE':
+            return currentCount*2;
+        case 'RESET':
+            return 0
+        default:
+            return currentCount
+    }
+}
 
 type CounterProps = {
     initialValue: number
@@ -6,13 +23,15 @@ type CounterProps = {
 
 const Counter = (props: CounterProps) => {
     const { initialValue } = props;
-    const [ count, setCount ] = useState(initialValue)
+    const [ count, dispatch ] = useReducer(reducer, initialValue)
 
     return (
         <div>
             <p>Count: {count}</p>
-            <button onClick={() => setCount(count-1)}>-</button>
-            <button onClick={() => setCount((prevCount) => prevCount+1)}>+</button>
+            <button onClick={() => dispatch('DECREMENT')}>-</button>
+            <button onClick={() => dispatch('INCREMENT')}>+</button>
+            <button onClick={() => dispatch('DOUBLE')}>*2</button>
+            <button onClick={() => dispatch('RESET')}>Reset</button>
         </div>
     )
 }
