ba7eafb9e7b9980f967cc5f95653f7a6951542da
rtake <rtake1110@gmail.com>
2023-07-31 13:16:09 +0900

    Buttonのpresentational componentを追加

---
+++ src/components/button.tsx
@@ -0,0 +1,19 @@
+type ButtonProps = {
+    label: string
+    text: string
+    disabled: boolean
+    onClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+export const Button = (props: ButtonProps) => {
+    const { label, text, disabled, onClick } = props
+
+    return (
+        <div className="button-container">
+            <span>{label}</span>
+            <button disabled={disabled} onClick={onClick}>
+                {text}
+            </button>
+        </div>
+    )
+}
